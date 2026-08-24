57ba720b4d1ca9a434b3ce235b6183f5567c894b
aajah <aajah@example.com>
2026-08-24 17:53:49 +0200

    Update app version to V1.3.3

--- birds_ai_sound_classifier.py
+++ birds_ai_sound_classifier.py
@@ -919,7 +919,7 @@ class AudioMonitor:
 # --- FLASK ROUTEN ---
 @app.context_processor
 def inject_version():
-    return dict(version="V1.3.3-RC2")
+    return dict(version="V1.3.3")
 
 @app.route('/favicon.ico')
 def favicon():
@@ -2590,7 +2590,7 @@ def check_model_update():
 
 @app.route('/api/check_app_update')
 def check_app_update():
-    current_version = "V1.3.3-RC2"
+    current_version = "V1.3.3"
     try:
         import urllib.request
         import json
